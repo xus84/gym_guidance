1663d03a1054c199551c152f9c7dd168c933d234
xus84 <g_garsan@hotmail.com>
2023-11-20 13:09:15 +0100

    First code review

--- src/app/exercises/ExerciseCard.js
+++ src/app/exercises/ExerciseCard.js
@@ -1,41 +1,44 @@
 "use client"
 import { useState } from 'react';
+
+{/* 
+ExerciseCard component displays an exercise card with name, description, and image.
+
+Props:
+- exercise: Object representing the exercise data
+- onSelect: Function called when the exercise card is clicked
+*/}
+
+ 
 const ExerciseCard = ({ exercise, onSelect }) => {
-    const { name, description, image } = exercise;
-    const [isClicked, setIsClicked] = useState(false);
-
-    const handleClick = () => {
-      setIsClicked(!isClicked);
-      onSelect(exercise);
-    };
-
-    
+  const { name, description, image } = exercise;
+  const [isClicked, setIsClicked] = useState();
   
-    return (
-        
-        <div className="bg-white max-h-[18rem] min-h-[18rem]"
-          onClick={handleClick}
-          style={{
-            border: `2px solid ${isClicked ? 'green' : 'gray'}`,
-            borderWidth: `${isClicked ? '4px' : '2px'}`,
-            padding: '10px',
-            margin: '10px',
-            cursor: 'pointer',
-          }
-
-          }> {/* Updated width class for lg */}
-          <div className="border border-gray-300 p-2 rounded-md">
-            <div className="flex flex-col items-center ">
-            <img className="w-full h-16 object-cover mb-2 rounded-md" src={image} alt={name} style={{ width: exercise.width, height: exercise.height }}/>
-            </div>
-            <h3 className="text-lg font-bold mb-2">{name}</h3>
-            <p className="text-gray-600 text-sm">{description}</p>
-            {/* Add a link to the individual exercise page if needed */}
-          </div>
-        </div>
-      
-    );
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+    onSelect(exercise);
   };
-  
-  export default ExerciseCard;
-  
+
+
+
+  return (
+    
+    <div
+    className={`bg-white max-h-[18rem] min-h-[18rem] ${
+      isClicked ? 'border-green-500 border-4' : 'border-gray-500 border-2'
+    } p-2 m-3 cursor-pointer`}
+    onClick={handleClick}
+    >
+      <div className="border border-gray-300 p-2 rounded-md">
+        <div className="flex flex-col items-center ">
+          <img className="w-full h-16 object-cover mb-2 rounded-md exercise-image" src={image} alt={name}/>
+        </div>
+        <h3 className="text-lg font-bold mb-2">{name}</h3>
+        <p className="text-gray-600 text-sm">{description}</p>
+        {/* Add a link to the individual exercise page if needed */}
+      </div>
+    </div>
+  );
+};
+
+export default ExerciseCard;
--- src/app/exercises/page.js
+++ src/app/exercises/page.js
@@ -1,20 +1,19 @@
 "use client"
-import React, { useEffect, useState } from 'react'
-
+import React, { useState } from 'react'
 import ExerciseCard from './ExerciseCard';
 import Header from '@/components/Header';
-
+import { exercises } from './ExerciseData';
 function Exercises() {
 
   const [selectedExercises, setSelectedExercises] = useState([]);
-  
+
 
   const handleExerciseClick = (exercise) => {
-   
+
     setSelectedExercises((prevExercises) => {
       const isAlreadySelected = prevExercises.some((selectedExercise) => selectedExercise.id === exercise.id);
 
-      if(!isAlreadySelected) {
+      if (!isAlreadySelected) {
         return [...prevExercises, exercise];
       } else {
         return prevExercises.filter((e) => e.id !== exercise.id);
@@ -22,93 +21,8 @@ function Exercises() {
     })
   };
 
-  useEffect(() => {
-    console.log(selectedExercises)
-  }, [selectedExercises])
-  
 
-  const exercises = [
-    {
-      id: 1,
-      name: 'Step Touch',
-      description: 'Step side to side, lightly tapping your foot to the side.',
-      image: './img/exercises/step_touch.jpg',
-      width: '150px',
-      height: '140px',
-    },
-    {
-      id: 2,
-      name: 'High Knees',
-      description: 'Jog in place while lifting your knees as high as possible.',
-      image: './img/exercises/step_touch.jpg',
-      width: '150px',
-      height: '140px',
-    },
-    {
-      id: 3,
-      name: 'Butt Kicker',
-      description: 'Jog in place, kicking your heels up towards your glutes.',
-      image: './img/exercises/step_touch.jpg',
-      width: '150px',
-      height: '140px',
-    },
-    {
-      id: 4,
-      name: 'Running in Place',
-      description: 'Simulate running while staying in one spot.',
-      image: './img/exercises/step_touch.jpg',
-      width: '150px',
-      height: '140px',
-    },
-    {
-      id: 5,
-      name: 'Front Kicks',
-      description: 'Alternate kicking your legs forward.',
-      image: './img/exercises/step_touch.jpg',
-      width: '150px',
-      height: '140px',
-    },
-    {
-      id: 6,
-      name: 'Windmill',
-      description: 'Stand with feet shoulder-width apart, arms outstretched.',
-      image: './img/exercises/step_touch.jpg',
-      width: '150px',
-      height: '140px',
-    },
-    {
-      id: 7,
-      name: 'Bend and Reach',
-      description: 'Stand with feet shoulder-width apart. Bend at the waist, reaching down towards your toes.',
-      image: './img/exercises/step_touch.jpg',
-      width: '150px',
-      height: '140px',
-    },
-    {
-      id: 8,
-      name: 'Calf Raises',
-      description: 'Lift your heels off the ground while standing.',
-      image: './img/exercises/step_touch.jpg',
-      width: '150px',
-      height: '140px',
-    },
-    {
-      id: 9,
-      name: 'Arm Circles',
-      description: 'Extend your arms to the sides and make circular motions with your hands.',
-      image: './img/exercises/step_touch.jpg',
-      width: '150px',
-      height: '140px',
-    },
-    {
-      id: 10,
-      name: 'Side Hops',
-      description: 'Hop from side to side, landing softly on the balls of your feet.',
-      image: './img/exercises/step_touch.jpg',
-      width: '150px',
-      height: '140px',
-    },
-  ];
+
 
   const selectedExercisesList = selectedExercises.map((exercise) => (
     <p key={exercise.id}>{exercise.name}</p>
@@ -119,19 +33,19 @@ function Exercises() {
     <div className="bg-red-200">
       <Header />
       <div>
-      <h2 className='text-2xl font-bold mt-2 text-center'>Create your own routine and click the timer</h2>
+        <h2 className='text-2xl font-bold mt-2 text-center'>Create your own routine and click the timer</h2>
       </div>
-      <div className="grid grid-cols-5 gap-4 p-3">
+      <div className="grid grid-cols gap-4 p-3 md:grid-cols-5 sm:grid-cols-3">
         {exercises.map((exercise, index) => (
           <div key={index}>
-          <ExerciseCard  exercise={exercise} onSelect={handleExerciseClick}/>
+            <ExerciseCard exercise={exercise} onSelect={handleExerciseClick} />
           </div>
         ))}
       </div>
 
-      <div className="mt-8 p-4">     
-      <h2 className="text-2xl font-bold mb-4 text-center">Selected Exercises</h2>
-       <ul className="text-1xl font-semibold list-disc pl-8">{selectedExercisesList}
+      <div className="mt-8 p-4">
+        <h2 className="text-2xl font-bold mb-4 text-center">Selected Exercises</h2>
+        <ul className="text-1xl font-semibold list-disc pl-8">{selectedExercisesList}
         </ul>
       </div>
     </div>
